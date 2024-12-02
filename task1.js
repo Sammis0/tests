241c784ef863acd8ae82dccceff01836f0e25c87
Sammis0 <samya.bahouth@gmail.com>
2024-12-02 19:29:24 +0200

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,4 @@
+const num=123;
+Number(num%2===0 || num%3===0 || num%5===0) && console.log('1');
+Number(num%2===0 && num%3===0 || num%3===0 && num%5===0 || num%2===0 && num%5===0) && console.log('2');
+Number(num%2===0 && num%3===0 && num%5===0) && console.log('3');
