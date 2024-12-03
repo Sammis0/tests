161b10c6b5406f637430d8dbf8aeba277b994948
Sammis0 <samya.bahouth@gmail.com>
2024-12-03 02:06:01 +0200

    Delete task1.js

--- task1.js
+++
@@ -1,4 +0,0 @@
-const num=123;
-Number(num%2===0 || num%3===0 || num%5===0) && console.log('1');
-Number(num%2===0 && num%3===0 || num%3===0 && num%5===0 || num%2===0 && num%5===0) && console.log('2');
-Number(num%2===0 && num%3===0 && num%5===0) && console.log('3');
